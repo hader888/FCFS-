425ddbd53b65d8940b487c71bee07b214db25961
hader888 <haderomar54@yahoo.com>
2016-07-30 17:19:17 +0200

    Add files via upload

---
+++ project-FCFS/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace project_FCFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(16, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Arrival";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Fuchsia;
+            this.label3.Location = new System.Drawing.Point(16, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Burst";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Fuchsia;
+            this.label4.Location = new System.Drawing.Point(27, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "The table";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(316, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(84, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Solve";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(118, 451);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Average of TA";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button4.Location = new System.Drawing.Point(118, 477);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Average of W";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Fuchsia;
+            this.label5.Location = new System.Drawing.Point(18, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "GanttChart";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 456);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(239, 482);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "label7";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(93, 167);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(429, 124);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Arrival";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Burst";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Start";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Finish";
+            this.columnHeader5.Width = 58;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Waiting";
+            this.columnHeader6.Width = 53;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Turn around";
+            this.columnHeader7.Width = 224;
+            // 
+            // listView3
+            // 
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView3.GridLines = true;
+            this.listView3.Location = new System.Drawing.Point(477, 12);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(179, 128);
+            this.listView3.TabIndex = 16;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Name";
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Arrival";
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Burst";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(555, 219);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(93, 313);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(563, 23);
+            this.listView2.TabIndex = 17;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView4
+            // 
+            this.listView4.Location = new System.Drawing.Point(118, 339);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(538, 21);
+            this.listView4.TabIndex = 18;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(81, 339);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "label8";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(93, 366);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 75);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GanttChart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(678, 507);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listView4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FCFS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ project-FCFS/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace project_FCFS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public ArrayList H = new ArrayList();
+        public void InsortionSort(ArrayList H)
+        {
+            Process HHH = new Process();
+            for (int l = 0; l < H.Count; l++)
+            {
+                for (int sorted = 0; sorted < H.Count - 1; sorted++)
+                {
+                    if (((Process)H[sorted]).Arrival > ((Process)H[sorted + 1]).Arrival)
+                    {
+                        HHH = (Process)H[sorted + 1];
+                        H[sorted + 1] = (Process)H[sorted];
+                        H[sorted] = HHH;
+                    }
+                }
+            }
+        }
+
+         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        
+        class Process
+        {
+           public string Name;
+           public int Arrival;
+           public int Burst;
+           public int Start;
+           public int Finish;
+           public int TurnAround;
+           public int Wait;
+           public Process(String Name, int Arrival, int Burst)
+           {
+               this.Name = Name;
+               this.Arrival = Arrival;
+               this.Burst = Burst;
+           }
+
+           public Process()
+           {
+               // TODO: Complete member initialization
+           }
+
+          
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process Object = new Process();
+            Object.Name = textBox1.Text;
+            Object.Arrival = int.Parse(textBox2.Text);
+            Object.Burst = int.Parse(textBox3.Text);
+            H.Add(Object);
+            string[] row = { textBox1.Text, textBox2.Text, textBox3.Text };
+            var ListViewItem = new ListViewItem(row);
+            listView3.Items.Add(ListViewItem);
+            foreach (Control x in this.Controls)
+            {
+                if (x is TextBox)
+                    x.Text = "";
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            Process Object = new Process();
+            InsortionSort(H);
+            for (int i = 0; i < H.Count; i++)
+            {
+                if (i == 0)
+                {
+                    ((Process)H[0]).Start = ((Process)H[0]).Arrival;
+                    ((Process)H[0]).Finish = (((Process)H[0]).Start) + (((Process)H[0]).Burst);
+                    ((Process)H[0]).Wait = (((Process)H[0]).Start) - (((Process)H[0]).Arrival);
+                    ((Process)H[0]).TurnAround = (((Process)H[0]).Finish) - (((Process)H[0]).Arrival);
+                }
+                else
+                {
+                    if (((Process)H[i]).Arrival <= ((Process)H[i - 1]).Finish)
+                    {
+                        ((Process)H[i]).Start = ((Process)H[i - 1]).Finish;
+                    }
+                    else
+                    {
+                        ((Process)H[i]).Start = ((Process)H[i]).Arrival;
+                    }
+
+
+                    ((Process)H[i]).Finish = (((Process)H[i]).Start) + (((Process)H[i]).Burst);
+                    ((Process)H[i]).TurnAround = (((Process)H[i]).Finish) - (((Process)H[i]).Arrival);
+                    ((Process)H[i]).Wait = (((Process)H[i]).TurnAround) - (((Process)H[i]).Burst);
+
+                }
+                label8.Text = ((Process)H[0]).Start.ToString();
+
+            }
+            for (int i = 0; i < H.Count; i++)
+            {
+                ListViewItem LVI = new ListViewItem(((Process)H[i]).Name);
+                LVI.SubItems.Add(((Process)H[i]).Arrival.ToString());
+                LVI.SubItems.Add(((Process)H[i]).Burst.ToString());
+                LVI.SubItems.Add(((Process)H[i]).Start.ToString());
+                LVI.SubItems.Add(((Process)H[i]).Finish.ToString());
+                LVI.SubItems.Add(((Process)H[i]).Wait.ToString());
+                LVI.SubItems.Add(((Process)H[i]).TurnAround.ToString());
+                listView1.Items.Add(LVI);
+                listView2.Items.Add(((Process)H[i]).Name);
+                listView4.Items.Add(((Process)H[i]).Finish.ToString());
+
+            }
+            int point_size = 1;
+            for (int l = 0; l < H.Count; l++)
+            {
+                Label point = new Label();
+                point.Location = new Point(point_size * 20, 40);
+                point.Text = ((Process)H[l]).Name;
+                point.BorderStyle = BorderStyle.Fixed3D;
+                point.Size = new Size(((Process)H[l]).Burst * 20, 40);
+                point.BackColor = Color.Brown;
+                groupBox1.Controls.Add(point);
+                point_size += ((Process)H[l]).Burst;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double AverageOfTurnAround;
+            double Sum = 0;
+            for (int i = 0; i < H.Count; i++)
+            {
+                Sum = Sum + ((Process)H[i]).TurnAround;
+            }
+
+            AverageOfTurnAround = Sum / (H.Count);
+            label6.Text = AverageOfTurnAround .ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double AverageOfWait;
+            double Sum = 0;
+            for (int i = 0; i < H.Count; i++)
+            {
+                Sum = Sum + ((Process)H[i]).Wait;
+            }
+
+            AverageOfWait = Sum / (H.Count);
+            label7.Text = AverageOfWait.ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView3.Items.Clear();
+            listView2.Items.Clear();
+            listView4.Items.Clear();
+            label8.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+        }
+    }
+}
